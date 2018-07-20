8d6cdfd56a3c2503e251167027156529ed66c241
Claire Pillet <clairenatacha.pillet@free.fr>
2018-07-20 14:46:16 +0200

    First Commit

---
+++ Portfolio/src/index.js
@@ -0,0 +1,6 @@
+// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+ReactDOM.render(React.createElement("p", null, "test"), document.getElementById("content"));
---
+++ Portfolio/src/index.tsx
@@ -0,0 +1,11 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+
+ReactDOM.render(
+    <p>test</p>,
+    document.getElementById("content")
+);
